106bc9380d2fa3dcb61c13f87386d302be7ea8a1
Saish <saishnotfound@gmail.com>
2026-09-01 20:47:54 +0530

    fix: add missing LaunchedEffect import in FileManagerScreen.kt

--- app/src/main/java/com/xtmanager/ui/FileManagerScreen.kt
+++ app/src/main/java/com/xtmanager/ui/FileManagerScreen.kt
@@ -58,6 +58,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
